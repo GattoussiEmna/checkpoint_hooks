1a00b59bccc7fa8628636047ebb130818db0772a
unknown <Gattoussi>
2022-04-03 10:56:01 +0200

    first commit

---
+++ src/component/MovieData/Data.js
@@ -0,0 +1,66 @@
+const MovieData = [
+    {
+        title : "Iron Man",
+        year: "2008",
+        rating : 5 ,
+        posterUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxW-R2BvxxYuM-YoOb4uOTFVVZhiD8oEtdiyZgCD4iMX2uEKYa",
+    },
+    {
+        title : "Iron Man 3",
+        year : "2013",
+        rating : 3 ,
+        posterUrl: "https://m.media-amazon.com/images/M/MV5BMjE5MzcyNjk1M15BMl5BanBnXkFtZTcwMjQ4MjcxOQ@@._V1_SX300.jpg",
+    },
+
+
+    {
+        title : "Spider Man",
+        year : "2002",
+        rating : 5 ,
+        posterUrl: "https://m.media-amazon.com/images/M/MV5BZDEyN2NhMjgtMjdhNi00MmNlLWE5YTgtZGE4MzNjMTRlMGEwXkEyXkFqcGdeQXVyNDUyOTg3Njg@._V1_SX300.jpg",
+    },
+
+    {
+        title : "Ant-Man",
+        year : "2015",
+        rating : 4 ,
+        posterUrl: "https://m.media-amazon.com/images/M/MV5BMjM2NTQ5Mzc2M15BMl5BanBnXkFtZTgwNTcxMDI2NTE@._V1_SX300.jpg",
+    },
+
+    
+    {
+        title : "The Amazing Spider-Man",
+        year : "2012",
+        rating : 3 ,
+        posterUrl: "https://m.media-amazon.com/images/M/MV5BMjMyOTM4MDMxNV5BMl5BanBnXkFtZTcwNjIyNzExOA@@._V1_SX300.jpg",
+    },
+
+     
+    {
+        title : "Man of Steel",
+        year : "2013",
+        rating : 3 ,
+        posterUrl: "https://m.media-amazon.com/images/M/MV5BMTk5ODk1NDkxMF5BMl5BanBnXkFtZTcwNTA5OTY0OQ@@._V1_SX300.jpg",
+    },
+
+    {
+        title : "Spider-Man: Homecoming",
+        year : "2017",
+        rating : 2 ,
+        posterUrl: "https://m.media-amazon.com/images/M/MV5BNTk4ODQ1MzgzNl5BMl5BanBnXkFtZTgwMTMyMzM4MTI@._V1_SX300.jpg",
+    },
+
+    {
+        title : "Spider-Man 3",
+        year : "2007",
+        rating : 2 ,
+        posterUrl: "https://m.media-amazon.com/images/M/MV5BYTk3MDljOWQtNGI2My00OTEzLTlhYjQtOTQ4ODM2MzUwY2IwXkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_SX300.jpg",
+    },
+
+
+
+
+
+   
+];
+export default MovieData 
